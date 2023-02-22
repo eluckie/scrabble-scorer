5d7ad7aa962545637dc707b93502eccaf4501b40
E Luckie <eluckie.d@gmail.com>
2023-02-22 12:42:00 -0700

    pass third test to add characters with 2 points to dictionary

--- ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
+++ ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
@@ -38,7 +38,7 @@ namespace ScrabbleScorer.Tests
     [TestMethod]
     public void ScoreClass_AddCharactersWorthTwoPointsToDictionary_Dictionary()
     {
-      Dictionary<char, int> letterPoints = new Dictionary<char, int>() {};
+      Dictionary<char, int> letterPoints = new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1} };
       letterPoints['D'] = 2;
       letterPoints['G'] = 2;
       CollectionAssert.AreEquivalent(new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2} }, letterPoints);
