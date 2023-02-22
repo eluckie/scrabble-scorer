fdf1cdc066693a0a7e8a9acb823fc43854145ce9
E Luckie <eluckie.d@gmail.com>
2023-02-22 12:49:24 -0700

    pass fifth test to add characters with 4 points to dictionary

--- ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
+++ ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
@@ -53,5 +53,16 @@ namespace ScrabbleScorer.Tests
       letterPoints['P'] = 3;
       CollectionAssert.AreEquivalent(new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3} }, letterPoints);
     }
+    [TestMethod]
+    public void ScoreClass_AddCharactersWorthFourPointsToDictionary_Dictionary()
+    {
+      Dictionary<char, int> letterPoints = new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3} };
+      letterPoints['F'] = 4;
+      letterPoints['H'] = 4;
+      letterPoints['V'] = 4;
+      letterPoints['W'] = 4;
+      letterPoints['Y'] = 4;
+      CollectionAssert.AreEquivalent(new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}, {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4} }, letterPoints);
+    }
   }
 }
