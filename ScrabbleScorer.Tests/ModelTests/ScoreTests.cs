1fd294be5cbfb8a2513d8f8a47a9b0e4cdf096da
E Luckie <eluckie.d@gmail.com>
2023-02-22 12:40:37 -0700

    pass second test to create Dictionary & third to add values

--- ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
+++ ScrabbleScorer.Tests/ModelTests/ScoreTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScrabbleScorer.Models;
+using System.Collections.Generic;
 
 namespace ScrabbleScorer.Tests
 {
@@ -12,6 +13,35 @@ namespace ScrabbleScorer.Tests
       Score newScore = new Score();
       Assert.AreEqual(typeof(Score), newScore.GetType());
     }
-
+    [TestMethod]
+    public void ScoreClass_CreateDictionaryToHoldCharacterScores_Dictionary()
+    {
+      Dictionary<char, int> letterPoints = new Dictionary<char, int>() {};
+      Assert.AreEqual(typeof(Dictionary<char, int>), letterPoints.GetType());
+    }
+    [TestMethod]
+    public void ScoreClass_AddCharactersWorthOnePointToDictionary_Dictionary()
+    {
+      Dictionary<char, int> letterPoints = new Dictionary<char, int>() {};
+      letterPoints['A'] = 1;
+      letterPoints['E'] = 1;
+      letterPoints['I'] = 1;
+      letterPoints['O'] = 1;
+      letterPoints['U'] = 1;
+      letterPoints['L'] = 1;
+      letterPoints['N'] = 1;
+      letterPoints['R'] = 1;
+      letterPoints['S'] = 1;
+      letterPoints['T'] = 1;
+      CollectionAssert.AreEquivalent(new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1} }, letterPoints);
+    }
+    [TestMethod]
+    public void ScoreClass_AddCharactersWorthTwoPointsToDictionary_Dictionary()
+    {
+      Dictionary<char, int> letterPoints = new Dictionary<char, int>() {};
+      letterPoints['D'] = 2;
+      letterPoints['G'] = 2;
+      CollectionAssert.AreEquivalent(new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2} }, letterPoints);
+    }
   }
 }
